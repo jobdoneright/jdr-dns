4358bb8774b5900bdd9a415ff2ca827fb18698be
Simon McCartney <simon@mccartney.ie>
2019-02-17 21:16:46 +0000

    terraform fmt & comments about other domains

--- terraform.tf
+++ terraform.tf
@@ -3,19 +3,25 @@ provider "aws" {}
 # jobdoneright.ie
 # jobdoneright.mobi
 # jobdoneright.co.uk
-#
+
 module "jobdoneright_ie" {
-  source = "github.com/jobdoneright/r53-gsuite-mx-cname"
+  source   = "github.com/jobdoneright/r53-gsuite-mx-cname"
   dns_zone = "jobdoneright.ie"
 }
 
 module "jobdoneright_co_uk" {
-  source = "github.com/jobdoneright/r53-gsuite-mx-cname"
+  source   = "github.com/jobdoneright/r53-gsuite-mx-cname"
   dns_zone = "jobdoneright.co.uk"
 }
 
 module "jobdoneright_mobi" {
-  source = "github.com/jobdoneright/r53-gsuite-mx-cname"
+  source   = "github.com/jobdoneright/r53-gsuite-mx-cname"
   dns_zone = "jobdoneright.mobi"
 }
+
+# enablingenergy.net
+# enablingenergy.org
+# greenislandcommunications.com
+# greenislandcommunications.net
+# hillsboroughparish.org.uk
 
